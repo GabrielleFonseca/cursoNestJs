83b20db0d673e709916869f39ef0585c06039860
Gabrielle Fonseca <gabrielle.fonseca@zenklub.com>
2024-06-26 13:52:38 -0300

    Seção 2: Refatora métodos do serviço de cursos para lidar com relacionamentos

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -2,26 +2,41 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { Course } from './entities/courses.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Tag } from './entities/tags.entity';
+import { CreateCourseDTO } from './dto/create-curse.dto';
+import { UpdateCourseDTO } from './dto/update-courses.dto';
 
 @Injectable()
 export class CoursesService {
   constructor(
     @InjectRepository(Course)
     private readonly coursesRepository: Repository<Course>,
+
+    @InjectRepository(Tag)
+    private readonly tagsRepository: Repository<Tag>,
   ) {}
 
-  createCourse(createCourseDTO: any) {
-    const course = this.coursesRepository.create(createCourseDTO);
+  async createCourse(createCourseDTO: CreateCourseDTO) {
+    const tags = await Promise.all(
+      createCourseDTO.tags.map((name) => this.preloadTagByName(name)),
+    );
+    const course = this.coursesRepository.create({
+      ...createCourseDTO,
+      tags: tags,
+    });
     return this.coursesRepository.save(course);
   }
 
   async findAllCourses() {
-    return this.coursesRepository.find();
+    return this.coursesRepository.find({
+      relations: ['tags'],
+    });
   }
 
   async findCourseById(id: number) {
     const course = await this.coursesRepository.findOne({
       where: { id: id },
+      relations: ['tags'],
     });
 
     if (!course) {
@@ -31,10 +46,16 @@ export class CoursesService {
     return course;
   }
 
-  async updateCourse(id: number, updateCourseDTO: any) {
+  async updateCourse(id: number, updateCourseDTO: UpdateCourseDTO) {
+    const tags =
+      updateCourseDTO.tags &&
+      (await Promise.all(
+        updateCourseDTO.tags.map((name) => this.preloadTagByName(name)),
+      ));
     const course = await this.coursesRepository.preload({
       ...updateCourseDTO,
       id: id,
+      tags: tags,
     });
 
     if (!course) {
@@ -54,5 +75,15 @@ export class CoursesService {
     }
 
     return this.coursesRepository.remove(course);
+  }
+
+  private async preloadTagByName(name: string): Promise<Tag> {
+    const tag = await this.tagsRepository.findOne({ where: { name: name } });
+
+    if (tag) {
+      return tag;
+    }
+
+    return this.tagsRepository.create({ name });
   }
 }
